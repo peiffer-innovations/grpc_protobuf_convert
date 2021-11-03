cd37fc3abd67656203f733a728dd7794b80263db
Jeff Peiffer <jpeiffer@gmail.com>
2021-11-02 21:46:25 -0400

    Comments

--- lib/grpc_protobuf_convert.dart
+++ lib/grpc_protobuf_convert.dart
@@ -4,12 +4,12 @@ import 'package:grpc_googleapis/google/protobuf.dart' as pb;
 /// Utility class that helps convert from common Dart classes to Protobuf
 /// standard classes.
 class GrpcProtobufConvert {
-  /// Converts a Protobuf Duration [value] to a Dart core Duration.
+  /// Converts a Protobuf [Duration] [value] to a Dart core [Duration].
   static Duration fromDuration(pb.Duration value) => Duration(
         milliseconds: (value.seconds.toInt() * 1000) + (value.nanos ~/ 1000),
       );
 
-  /// Converts a Protobuf Timestamp [value] to a Dart core DateTime.
+  /// Converts a Protobuf [db.Timestamp] [value] to a Dart core DateTime.
   static DateTime fromTimestamp(
     pb.Timestamp value, {
     bool isUtc = true,
@@ -19,7 +19,8 @@ class GrpcProtobufConvert {
         isUtc: isUtc,
       );
 
-  /// Converts a Dart core Duration [value] to a Protobuf Duration object.
+  /// Converts a Dart core [Duration] [value] to a Protobuf [pb.Duration]
+  /// object.
   static pb.Duration toDuration(Duration value) {
     var seconds = value.inSeconds;
     var nanos =
@@ -36,7 +37,8 @@ class GrpcProtobufConvert {
   /// int values on Web only contain 53 significant bits vs the native 64 bits.
   static Int64 toInt64(int value) => Int64.parseInt(value.toString());
 
-  /// Converts a Dart core DateTime [value] to a Protobuf Timestamp object.
+  /// Converts a Dart core [DateTime] [value] to a Protobuf [db.Timestamp]
+  /// object.
   static pb.Timestamp toTimestamp(DateTime value) {
     var seconds = value.millisecondsSinceEpoch ~/ 1000;
     var nanos = (value.millisecondsSinceEpoch - (seconds * 1000)) * 1000;
